6797118179bf312417d3c58be7b13333e15836d9
Martina Cacciola <127241970+martinacacciola@users.noreply.github.com>
2024-10-31 16:00:27 +0100

    focusing on delta evolution

--- cumulative_A.py
+++ cumulative_A.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 df = pd.read_csv('./Brutus data/plummer_triples_L0_00_i1775_e90_Lw392.csv')
 
 forward_trajectory = df[df['Phase'] == 1]
@@ -11,9 +10,11 @@ backward_trajectory = df[df['Phase'] == -1]
 total_timesteps = len(df['Timestep'].unique())
 midpoint = total_timesteps // 2  # Midpoint corresponds to t=T=lifetime
 timesteps = df['Timestep'].unique()
-# Not including the midpoint
+
+# Define symmetric timesteps
 timesteps_forward = timesteps[:midpoint]
-timesteps_backward = timesteps[midpoint+1:]
+timesteps_backward = timesteps[midpoint+1:][::-1]  # Reverse order for symmetry
+print(len(timesteps_forward), len(timesteps_backward))
 
 particles = df['Particle Number'].unique()
 
@@ -32,86 +33,114 @@ def compute_delta(forward_state, backward_state):
     diff_vel = (vx_f - vx_b)**2 + (vy_f - vy_b)**2 + (vz_f - vz_b)**2
     diff_pos = (x_f - x_b)**2 + (y_f - y_b)**2 + (z_f - z_b)**2
     
-    return np.sum(diff_vel + diff_pos)
+    return diff_vel + diff_pos
 
-# We want to compute delta btw backward and forward solutions at each integration step
-# We need to consider the states symetrically around the midpoint
+# Flip the order of the backward trajectory
+#backward_trajectory = backward_trajectory.iloc[::-1]
+#print('Backward traj head:',backward_trajectory.head())
 
-# backward state = we need to flip the array
-backward_trajectory = backward_trajectory.iloc[::-1]
-print(backward_trajectory.head())
-# forward state = current forward state (until midpoint)
+# Compute delta at each symmetric timestep
 delta_per_step = []
 
-for i in range(len(timesteps_forward)):  
+for i in range(len(timesteps_forward)):
     delta_sum = 0  
     for p in particles:
         forward_p = forward_trajectory[forward_trajectory['Particle Number'] == p]
         backward_p = backward_trajectory[backward_trajectory['Particle Number'] == p]
         
         forward_state = forward_p[forward_p['Timestep'] == timesteps_forward[i]]
-        backward_state = backward_p[backward_p['Timestep'] == timesteps_backward[i]] 
-        # check first forward and backward states that are compared
-        if i == 0 and p == particles[0]:  # Only for the first timestep and first particle
-            print("First forward state compared:", forward_state)
-            print("First backward state compared:", backward_state)
-    
+        backward_state = backward_p[backward_p['Timestep'] == timesteps_backward[i]]
         
+        # check the comparison - seems correct
+        if i == 30 and p == particles[0]:  # check for a timestep and for a particle
+            print("Forward state compared:", forward_state)
+            print("Backward state compared:", backward_state)
+    
         delta = compute_delta(forward_state, backward_state)
         delta_sum += delta
         
-    delta_per_step.append(delta_sum)
-
- # Append the delta summed over the bodies for this timestep
-#print(delta_per_step)
+    delta_per_step.append(delta_sum) # Append the delta summed over the bodies for this timestep
+    #print(delta_per_step)
 
 # We want to compute delta between initial and final states
 delta_initial_final = 0
+
+initial_timestep = timesteps[0]  # start of forward trajectory
+final_timestep = timesteps[-1]   # end of backward trajectory
 
 for p in particles:
     forward_p = forward_trajectory[forward_trajectory['Particle Number'] == p]
     backward_p = backward_trajectory[backward_trajectory['Particle Number'] == p]
     
-    # Initial state from forward trajectory
-    forward_initial = forward_p[forward_p['Timestep'] == 0]
-    
-    # Final state from backward trajectory
-    backward_final = backward_p[backward_p['Timestep'] == timesteps_backward[-1]]
-    
-    delta_initial_final += compute_delta(forward_initial, backward_final) #not sure about +-
+    forward_initial = forward_p[forward_p['Timestep'] == initial_timestep]
+    backward_final = backward_p[backward_p['Timestep'] == final_timestep]
+  
+    delta_initial_final += compute_delta(forward_initial, backward_final) # Sum over the three particles
+print('delta initial final:', delta_initial_final)
 
-# Amplification factor for each integration step
-A = [delta_initial_final/delta_per_step[i] for i in range(len(delta_per_step))]
 
 # Crossing time 
 T_c = 2 * np.sqrt(2)
 # Normalize lifetime 
 T_norm = [timesteps[i] / T_c for i in range(len(delta_per_step))]
 
-# Amplification factor evolution over lifetime
+# Phase-space distance over lifetime
 plt.figure(figsize=(10, 6))
-plt.plot(T_norm, np.log10(A), color='b', alpha=0.5)
+plt.plot(T_norm, delta_per_step, color='b', alpha=0.5)
 plt.xlabel(r'$T/T_c$')
-plt.ylabel(r'$\log_{10}(A)$')
+plt.ylabel(r'$\delta$')
 plt.xlim(0, 100)
+#plt.yscale('log')
 #plt.ylim(0, 60)
 plt.grid(True)
-plt.savefig('./figures/amplification_factor.png')
+plt.savefig('./figures/phasespacedist.png')
 
-# Cumulative sum of the amplification factor (A)
-cumulative_A = np.cumsum(A)
+# Cumulative sum of the phase-space distance (delta)
+# Flip the order to measure separation from beginning of both trajectories
+cumulative_delta = np.cumsum(np.flip(delta_per_step))
 
-# Cumulative distribution of the amplification factor over time  
+# Cumulative distribution of delta over time  
 plt.figure(figsize=(10, 6))
-plt.plot(T_norm, cumulative_A, color='g', alpha=0.7, label="A")
+plt.plot(T_norm, cumulative_delta, color='g', alpha=0.7, label=r"$\delta$")
 plt.xlabel(r'$T/T_c$')
-plt.ylabel(r'$\log_{10}(A)$')
-plt.title('Cumulative Distribution of Amplification Factor Over Time')
+plt.ylabel(r'$\delta$')
+plt.title('Cumulative Distribution of Delta')
 plt.grid(True)
+plt.yscale('log')
 #plt.semilogy()
 plt.legend()
-plt.savefig('./figures/cumulative_A.png') 
+plt.savefig('./figures/cumulative_delta.png') 
 plt.show()
+
+# Amplification factor for each integration step
+A = [delta_initial_final/np.flip(delta_per_step[i]) for i in range(len(delta_per_step))]
+
+# Amplification factor over lifetime
+plt.figure(figsize=(10, 6))
+plt.plot(T_norm, A, color='b', alpha=0.5)
+plt.xlabel(r'$T/T_c$')
+plt.ylabel(r'$\delta$')
+plt.xlim(0, 100)
+#plt.yscale('log')
+#plt.ylim(0, 60)
+plt.grid(True)
+plt.title('Amplification Factor')
+#plt.show()
+#plt.savefig('./figures/A.png')
+
+cumulative_A = np.cumsum(A)
+# Cumulative distribution of the amplification factor over time  
+plt.figure(figsize=(10, 6))
+plt.plot(T_norm, cumulative_A, color='g', alpha=0.7, label='A')
+plt.xlabel(r'$T/T_c$')
+plt.ylabel(r'$\delta$')
+plt.title('Cumulative Distribution of A')
+plt.grid(True)
+plt.yscale('log')
+#plt.semilogy()
+plt.legend()
+#plt.savefig('./figures/cumulative_A.png') 
+#plt.show()
 
 
 
