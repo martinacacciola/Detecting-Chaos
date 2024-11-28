5cbc7d0daa3d5361cf1d3f8ee36036d6ad0e749f
Martina Cacciola <127241970+martinacacciola@users.noreply.github.com>
2024-11-28 16:48:28 +0100

    Add files via upload

--- test2.py
+++ test2.py
@@ -12,7 +12,7 @@ class TestPreprocessTrajectory(unittest.TestCase):
         self.gaussian_fit_params_path = './data/gmm_parameters_L0_00_i1775_e90_Lw392.txt'
 
     def test_preprocess_trajectory(self):
-        positions_velocities, timesteps, deltas, window_slopes, gaussian_fit_params = preprocess_trajectory(
+        forward_trajectories, backward_trajectories, timesteps, deltas, window_slopes, gaussian_fit_params = preprocess_trajectory(
             self.file_path,
             window_size=40,
             delta_per_step_path=self.delta_per_step_path,
@@ -21,17 +21,23 @@ class TestPreprocessTrajectory(unittest.TestCase):
         )
         
         # Check the output types
-        self.assertIsInstance(positions_velocities, np.ndarray)
+        self.assertIsInstance(forward_trajectories, dict)
+        self.assertIsInstance(backward_trajectories, dict)
         self.assertIsInstance(timesteps, np.ndarray)
         self.assertIsInstance(deltas, np.ndarray)
         self.assertIsInstance(window_slopes, np.ndarray)
+        self.assertIsInstance(gaussian_fit_params, np.ndarray)
         
-        # Check the output shapes
-        print('positions_velocities:', positions_velocities[:5])
-        print('shape of input:', positions_velocities.shape)
-        self.assertEqual(positions_velocities.shape[1], 6)  # 6 for each particle, 3 particles
-        #self.assertEqual(len(timesteps), positions_velocities.shape[0])
-        #self.assertEqual(len(deltas), positions_velocities.shape[0])
+        # Check the output shapes and contents
+        for particle in forward_trajectories:
+            self.assertIsInstance(forward_trajectories[particle], np.ndarray)
+            self.assertIsInstance(backward_trajectories[particle], np.ndarray)
+            self.assertEqual(forward_trajectories[particle].shape[1], 6)  # 6 for each particle, 3 particles
+            self.assertEqual(backward_trajectories[particle].shape[1], 6)  # 6 for each particle, 3 particles
+
+       # self.assertEqual(len(timesteps), len(deltas))
+       # self.assertEqual(len(timesteps), len(window_slopes))
+       # self.assertEqual(len(timesteps), len(gaussian_fit_params))
     
 if __name__ == '__main__':
     unittest.main()
